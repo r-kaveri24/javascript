a61e9013446c2f33938898e0a4b5709cf337672b
r-kaveri24 <109783031+r-kaveri24@users.noreply.github.com>
2024-08-22 12:34:02 +0000

    number conversion

---
+++ 01_basics/03_datatypes_conversion.js
@@ -0,0 +1,53 @@
+// let score = 33
+// console.log(typeof(score));
+// let valueOfNumber=Number(score)
+// console.log(typeof(score));
+// console.log(score);
+
+// let score = "33"
+// console.log(typeof(score));
+// let valueOfNumber=Number(score)
+// console.log(typeof(valueOfNumber));
+// console.log(valueOfNumber);
+
+// let score = "33AS"
+// console.log(typeof(score));
+// let valueOfNumber=Number(score)
+// console.log(typeof(valueOfNumber));
+// console.log(valueOfNumber);
+
+// let score = null
+// console.log(typeof(score));
+// let valueOfNumber=Number(score)
+// console.log(typeof(valueOfNumber));
+// console.log(valueOfNumber);
+
+let score = undefined
+// console.log(typeof(score));
+let valueOfNumber=Number(score)
+// console.log(typeof(valueOfNumber));
+// console.log(valueOfNumber);
+
+/*
+
+"33"- 33
+"33WW"= nan
+null=0
+undefined = nan
+true-1 and false-0
+
+*/ 
+let isLoggedIn = " "
+let isBooleanLoogedIn = Boolean(isLoggedIn)
+// console.log(isBooleanLoogedIn);
+/*
+1=>true 0=>false
+""=>false
+" ka"=>true
+*/ 
+
+let isNumber = 33
+let stringNumber = String(isNumber)
+console.log(stringNumber );
+console.log(typeof stringNumber);
+

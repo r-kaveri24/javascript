cae8433a2a591f673182ee39656947153399b033
r-kaveri24 <109783031+r-kaveri24@users.noreply.github.com>
2024-08-22 14:04:39 +0000

    interview qustions on the datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,21 @@
+/*there are two types of datatypes based on how we call them and how we store them
+1. Primitive- call by value,there is reference provided when we call the value only the copy is provided  and we make changes to the copy
+
+
+// there are 7: String,boolean,number,null,undefined,symbol(when want to make something unique),Bigint
+
+
+
+
+2. Non-Primitive or Reference Type-when we are given reference value when called upon
+//there total 3- array,function,objects
+*/
+
+
+
+// javascript is the (dynamicaly typed) language means it can change the value at the runtime.
+
+
+
+
+find the type of all the datatypes exercise//

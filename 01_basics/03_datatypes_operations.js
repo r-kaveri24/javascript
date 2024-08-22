7cbe03c1fe029fe3eaef1bc2ac33545e1bc5f3f2
r-kaveri24 <109783031+r-kaveri24@users.noreply.github.com>
2024-08-22 13:17:09 +0000

    operation doned on datatypes

---
+++ 01_basics/03_datatypes_operations.js
@@ -0,0 +1,24 @@
+let isValue = 3
+let negValue = -isValue
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/2);
+// console.log(2%2);
+
+// operation on stings
+
+let st1="kaveri"
+let str2 = " raut"
+let str3=(st1+str2)
+console.log(str3);//kaveri raut
+
+console.log(2+3);//5
+console.log("2"+"3");//23
+console.log(2+"3");//23
+console.log("2"+3);//23
+console.log("2"+3 + 2);//223
+console.log(2+2+"3");//43

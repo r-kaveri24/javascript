b8ae646e1e4a8bffd4e7d3fb16cdb7cbb6ffcb9a
r-kaveri24 <109783031+r-kaveri24@users.noreply.github.com>
2024-08-22 13:36:21 +0000

    comparision and what to avoid while doing it

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+
+// console.log("2" > 1);
+
+// console.log("02" > 1);
+
+
+
+
+console.log(null >0); // false
+console.log(null<0);// false
+console.log(null>=0); //true
+
+// the comparision and equality operater works differently for the null the comparision operater convertes it to number(0) thats why null>=0 gives true
+
+
+// === strict check (not only checks the value it also checks the datatype of the value)

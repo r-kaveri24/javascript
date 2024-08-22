a2f8cfe334fd366e0be196fd8f2538373bd09065
r-kaveri24 <109783031+r-kaveri24@users.noreply.github.com>
2024-08-22 12:08:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all the js code as newer version
+// alert(3+2) //we are using the node.js, not browser
+
+
+console.log(0
+    +7
+);//code radability should be high
+
+/* Data type
+
+1. Numbers- 0 tp 58
+2.Bigint - 
+3. Boolean - true or false
+4. String - ""
+5. null - standalone value
+6. undefinded - 
+7. symbol - unique
+
+8. object
+
+
+*/ 
+
+// interview qustion
+console.log(typeof null);
+console.log(typeof undefined);
+

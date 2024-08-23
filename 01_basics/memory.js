464b207d856ccc57db60eb1db03dadfb40494821
r-kaveri24 <109783031+r-kaveri24@users.noreply.github.com>
2024-08-23 07:03:02 +0000

    how copy and reference variables are stored in the memory

---
+++ 01_basics/memory.js
@@ -0,0 +1,17 @@
+//Stack(primitive)  Heap(Non-primitive)
+
+let k="kaveri"
+let r =k
+r="rajesh"
+console.log(k);
+console.log(r);
+
+let b ={
+    email: "raut@google.com",
+    name: "rajesh"
+}
+let m=b
+m.email="mom@gmail.com"
+console.log(b);
+console.log(m);
+

77d06a3f350429846f55fa7ec522b17dc13f21fe
r-kaveri24 <109783031+r-kaveri24@users.noreply.github.com>
2024-08-23 08:56:18 +0000

    string methods

---
+++ 01_basics/05_string.js
@@ -0,0 +1,13 @@
+const life="lifeline"
+const line ="why happend"
+console.log(life+line); // dont use this method this is outdate
+console.log(`why this haaped ${life} and why should happen ${line}`); // use this beacuse it is more readable and understandable and easy
+
+
+
+let gameName= new String("kaveri") //a way of creating the string with object type
+
+//string methods learn about them.
+
+
+
